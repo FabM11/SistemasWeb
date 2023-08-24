d76bc691aeb0a8e60a356a5fd8b8adfe456b3882
Fabrício <flechainbody@gmail.com>
2023-08-24 11:32:38 -0300

    simulação da tabela

---
+++ tabela banco de dados/simulaçãotabela.php
@@ -0,0 +1,26 @@
+<?php
+
+$tabelaclientes = 
+  [
+1 => [
+  'Nome' => 'Fabrício',
+  'Cidade' => 'Jundiaí',
+  'Idade' => 17
+  ],
+  
+2 => [
+  'Nome' => 'Davi',
+  'Cidade' => 'Jundiaí',
+  'Idade' => 17
+],
+3 => [
+    'Nome' => 'Fernando',
+    'Cidade' => 'Franco da Rocha',
+    'Idade' => 'Incríveis 43 aninhos'
+]
+  ];
+
+//Procurar na tabela de um banco de dados usando o id e o termo de procura
+  echo $tabelaclientes [1]['Cidade'];
+
+?>
