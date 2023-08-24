29861ac6be01d6cd0d6eaa468b07bf1e1e109b99
Fabrício <flechainbody@gmail.com>
2023-08-24 12:28:20 -0300

    foreach

---
+++ tabela banco de dados/simulçaoProcuraForEach.php
@@ -0,0 +1,17 @@
+<?php
+$matrix = [[1,2,9,4,5],[10,20,30,40,50],[100,200,300,400,500],[1000,2000,3000,4000,5000,[5000]]];
+
+for($i=0; $i < 4; $i++){
+  for($n = 0; $n < 4; $n ++){
+    if ($i==3 && $n==4){
+    echo "......ultimo vetor...... \n";
+    for($c=0; $c<4; $c++){
+
+    }
+    }
+      echo "posição matrix".$i." e posição dentro do vetor ".$n." e o conteudo é: ". $matriz[$i][$n]. PHP_EOL;
+  }
+}
+$matrix[3][4][2]=null;
+echo "valor ".$matrix[3][4][0]." ";
+print_r($matrix);
